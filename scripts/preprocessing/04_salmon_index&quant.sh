e70841ebdd774626a74f30d9446255d9e49883b6
ClaireCurel <clairecurel@gmail.com>
2026-04-16 10:41:05 +0100

    Create 04_salmon_index&quant.sh

---
+++ scripts/preprocessing/04_salmon_index&quant.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+salmon index \
+  -t Mus_musculus.GRCm39.cdna.all.fa.gz \
+  -i Mus_musculus.GRCm39_index
+  DIR=$1
+INDEXDIR="/path/to/index"
+SALMON="/path/to/salmon"
+
+for file in $DIR/*_R1_*.fastq.gz
+do
+    sample=$(basename $file _R1_001_trimmed.fastq.gz)
+
+    $SALMON quant \
+      -i $INDEXDIR \
+      -l A \
+      -1 $DIR/${sample}_R1_001_trimmed.fastq.gz \
+      -2 $DIR/${sample}_R2_001_trimmed.fastq.gz \
+      -p 16 \
+      --validateMappings \
+      -o quants/${sample}
+done
