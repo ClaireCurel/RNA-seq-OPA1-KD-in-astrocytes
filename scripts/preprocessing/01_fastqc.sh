465c7f6ba11f6701f42a088f9f5ee32bcdf0af8c
ClaireCurel <clairecurel@gmail.com>
2026-04-16 10:38:03 +0100

    Create 01_fastqc.sh

---
+++ scripts/preprocessing/01_fastqc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT_DIR=$1
+OUTDIR=$PROJECT/fastqc
+mkdir -p $OUTDIR
+
+module load fastqc/v0.11.9
+
+for fastq_file in $INPUT_DIR/*.fastq.gz
+do
+    fastqc $fastq_file -o $OUTDIR
+done
