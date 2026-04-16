22f64755b84101b7148048947a0b2146698e4167
ClaireCurel <clairecurel@gmail.com>
2026-04-16 10:42:02 +0100

    Create 05_samtools.sh

---
+++ scripts/preprocessing/05_samtools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INPUTDIR=$1
+cd $INPUTDIR
+
+for sam_file in *.sam
+do
+    bam_file="${sam_file%.sam}.bam"
+    sorted="${sam_file%.sam}_sorted.bam"
+
+    samtools view -b -o "$bam_file" "$sam_file"
+    samtools sort "$bam_file" -o "$sorted"
+    samtools index "$sorted"
+done
